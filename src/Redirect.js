957139aef771d7fb8a142295e59c72016ca2c41d
Zach Izzard <zachizzard@gmail.com>
2023-01-08 01:05:47 -0500

    Updating to initial state

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,13 +1,18 @@
 import { useEffect } from 'react';
+import { useLocation } from "react-router-dom"
+
+const useName = () => {
+    const location = useLocation();
+    return location.pathname.split("/")[1];
+  }
 
 
 function Redirect ({to}) {
-
+    const name = useName();
     useEffect(() => {
-        // Update the document title using the browser API
+        document.title = name + "'s holiday card <3";
         window.location = to;
     });
-
 
   return (
     <></>
